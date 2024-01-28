7a66c172a8b6f919b9c62bc71bc5de08a31d0308
ridaiqianhe <121543611+ridaiqianhe@users.noreply.github.com>
2024-01-29 00:22:15 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -181,8 +181,8 @@ make_conf() {
                 else
                     echo "proxy_pass $domain;"
                 fi)
-                proxy_set_header X-Real-IP $remote_addr;
-                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
                 proxy_cache_bypass \$http_upgrade;
 
                 proxy_cache_valid 200 $cache_time;
